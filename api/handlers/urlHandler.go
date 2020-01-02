7c5ddbabd7fc3044299a7bcfcc8422eb2a66d209
Austin Williams <awilliams17412@gmail.com>
2020-01-01 19:31:58 -0600

    Added urlHandler Added CreateUUIDFromURL and GetURLFromUUID method stubs

---
+++ api/handlers/urlHandler.go
@@ -0,0 +1,13 @@
+package handlers
+
+import (
+	"github.com/labstack/echo"
+)
+
+func CreateUUIDFromURL(c echo.Context) error {
+
+}
+
+func GetURLFromUUID(c echo.Context) error {
+
+}
--- api/router.go
+++ api/router.go
@@ -1,12 +1,16 @@
 package api
 
 import (
+	"github.com/awilliams17/ShortThis/api/handlers"
 	"github.com/labstack/echo"
 )
+
 
 func CreateRouter() *echo.Echo {
 	e := echo.New()
 
+	e.GET("/api/redirect", handlers.GetURLFromUUID)
+	e.POST("/api/create_uuid_key", handlers.CreateUUIDFromURL)
+
 	return e
 }
-
